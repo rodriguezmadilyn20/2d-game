a95efcecb85bcad57637daf60dca41fb38f7697c
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2026-04-29 17:05:04 +0800

    add audio and pause screen

--- scripts/ui/v_box_container.gd
+++ scripts/ui/v_box_container.gd
@@ -1,6 +1,10 @@
 extends VBoxContainer
+
 @onready var mainMenu = $"."
 @onready var options = $"../OptionsPanel"
+
+var audio_bus_id = AudioServer.get_bus_index("Audio")
+var sfx_bus_id = AudioServer.get_bus_index("SFX")
 
 func _ready()-> void:
 	mainMenu.show()
@@ -21,3 +25,22 @@ func _on_options_pressed() -> void:
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+
+
+
+func _on_close_options_btn_pressed() -> void:
+	mainMenu.visible = true
+	options.visible = false
+	
+
+
+func _on_audio_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(audio_bus_id, linear_to_db(value))
+	
+
+
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(sfx_bus_id, linear_to_db(value))
