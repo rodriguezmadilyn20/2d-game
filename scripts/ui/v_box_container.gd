11a0b479bbb0bb3a456f655d22b9a5fb10374a84
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2026-04-29 16:54:09 +0800

    add autoplay and container

--- scripts/ui/v_box_container.gd
+++ scripts/ui/v_box_container.gd
@@ -1,5 +1,10 @@
 extends VBoxContainer
+@onready var mainMenu = $"."
+@onready var options = $"../OptionsPanel"
 
+func _ready()-> void:
+	mainMenu.show()
+	options.hide()
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Scene1.tscn")
@@ -10,7 +15,8 @@ func _on_load_pressed() -> void:
 
 
 func _on_options_pressed() -> void:
-	pass # Replace with function body.
+	mainMenu.hide()
+	options.show()
 
 
 func _on_exit_pressed() -> void:
